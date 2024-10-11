12ee9942edfa2284517e64d308290afb34ac5501
rosser <ross@homeincrozier.com>
2024-10-10 21:14:06 -0400

    added javascript to create and store array in localstorage

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,3 +7,12 @@ checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
   else
     checkList.classList.add('visible');
 }
+let restaurantlist = []
+
+function addnewrestaurant() {
+  const restaurantname = document.getElementById('enter-restaurant');
+  const inputrestaurant = restaurantname.value;
+  restaurantlist.push(inputrestaurant).value;
+  localStorage.setObj(restaurantlist, JSON.stringify(restaurantlist))
+  console.log(restaurantlist);
+}
